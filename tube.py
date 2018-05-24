160ca609ba6fb69251e98bacd4ccabdaffff81b2
John Mildinhall <john_mildinhall@hotmail.com>
2018-05-24 16:22:09 +0100

    rotate image

--- tube.py
+++ tube.py
@@ -5,7 +5,7 @@ from PIL import ImageFont
 def draw_text(position, text, font=None, colour=inkyphat.BLACK, rotation=0):
     x, y = position
     if font is None:
-        font = inkyphat.ImageFont.truetype(inkyphat.fonts.PressStart2P,12)
+        font = inkyphat.ImageFont.truetype(inkyphat.fonts.FredokaOne,12)
     w, h = font.getsize(text)
     mask = inkyphat.Image.new('1', (w, h))
     draw = inkyphat.ImageDraw.Draw(mask)
@@ -27,5 +27,5 @@ font_file = inkyphat.fonts.FredokaOne
 font_size = 10
 font = inkyphat.ImageFont.truetype(font_file, font_size)
 text = "hello"
-draw_text((80, 20), text, rotation=90)
+draw_text((20, 20), text, rotation=90)
 inkyphat.show()
