ab65640e7efa4dcd6c353aa53d00cab1c9743058
John Mildinhall <john_mildinhall@hotmail.com>
2018-05-24 16:20:31 +0100

    rotate image

--- tube.py
+++ tube.py
@@ -1,6 +1,18 @@
 import requests, json, inkyphat
 from PIL import ImageFont
 
+#function to rotate text
+def draw_text(position, text, font=None, colour=inkyphat.BLACK, rotation=0):
+    x, y = position
+    if font is None:
+        font = inkyphat.ImageFont.truetype(inkyphat.fonts.PressStart2P,12)
+    w, h = font.getsize(text)
+    mask = inkyphat.Image.new('1', (w, h))
+    draw = inkyphat.ImageDraw.Draw(mask)
+    draw.text((0, 0), text, 1, font)
+    mask = mask.rotate(rotation, expand=True)
+    inkyphat.paste(colour, position, mask)
+
 url = 'https://api.tfl.gov.uk/line/mode/tube/status'
 response = requests.get(url)
 data = json.loads(response.text)
@@ -15,5 +27,5 @@ font_file = inkyphat.fonts.FredokaOne
 font_size = 10
 font = inkyphat.ImageFont.truetype(font_file, font_size)
 text = "hello"
-inkyphat.text((0, top), text, 2, font=font)
+draw_text((80, 20), text, rotation=90)
 inkyphat.show()
